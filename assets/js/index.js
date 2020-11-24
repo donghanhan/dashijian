9eb748bbd3890d49539843643ccb7242a135ed46
zhangzhendong <1329437030@qq.com>
2020-11-24 20:09:39 +0800

    adsadsa

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,18 @@
 $(function(){
     gitUserInfo()
-})
 
+    var layer =layui.layer
+
+    $('#toover').on('click',function(){
+        layer.confirm('确定退出登陆?', {icon: 3, title:'提示'}, function(index){
+            //do something
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+          })
+    })
+})
+//获取用户基本信息
 function gitUserInfo() {
     $.ajax({
         type: 'GET',
@@ -14,7 +25,13 @@ function gitUserInfo() {
                 return layui.layer.msg(res.message)
             }
             renderAvatar(res.data)
-        }
+        },
+        // complete:  function(res){
+        // if(res.responseJSON.status === 1&&res.responseJSON.message === '身份认证失败！'){
+        //     localStorage.removeItem('token')
+        //     location.href='login.html'
+        // }
+        // }
     })
 }
 function renderAvatar(user){
